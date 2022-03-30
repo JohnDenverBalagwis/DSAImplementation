4b06f312164dcf9334847baf0271a7e023ca5b4a
JD <imba_jhey@yahoo.com>
2022-03-30 19:05:30 +0800

    Add files via upload

---
+++ QueueImplementation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueImplementation
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Queue myQueue = new Queue();
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+            myQueue.Enqueue(4);
+            myQueue.Enqueue(5);
+            myQueue.Enqueue(6);
+            Console.Write("Original queue: ");
+            foreach (int number in myQueue)
+            {
+                Console.Write(number + " ");
+            }
+            myQueue.Dequeue();
+            myQueue.Dequeue();
+            Console.WriteLine();
+            Console.Write("\nQueue after Dequeue operation: ");
+            foreach (int number in myQueue)
+            {
+                Console.Write(number + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
